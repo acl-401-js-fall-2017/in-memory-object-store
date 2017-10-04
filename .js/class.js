afb002505878dbf28e9544bd33a056a0f791dc4f
ckrountree <jmail0207@gmail.com>
2017-10-03 23:03:20 -0700

    add files and packages

---
+++ .js/class.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// class Store blueprint (collection of tools)
+
+class Store {
+    constructor() {
+        this.list = [];
+    }
+
+    save(object) {
+        this.list.push(object)
+    }
+}
