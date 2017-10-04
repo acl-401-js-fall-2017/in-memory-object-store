b50ca719a9d79110fb077c02e16d19a09e44c341
Shane Moyo <smoyo@pdx.edu>
2017-10-04 09:58:45 -0700

    make store test passing

---
+++ test/ObjectStore.test.js
@@ -0,0 +1,24 @@
+//const shortid = require('shortid');
+const assert = require('assert');
+
+// const obj1 = {
+//     name: "shane"
+// };
+
+
+class Store {
+    constructor() {
+        this.list = [];
+    }
+}
+
+let store = null; 
+beforeEach(function () {//Creates store unique object before each test.
+    store = new Store();
+});
+
+describe('Make Store', () => {
+    it('Makes new store', () => {
+        assert.deepEqual(store.list, []);
+    });
+});
