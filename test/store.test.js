ad22016cb5f4e1f495de66e94cdb7c3675d51644
David Nishikawa <triptype@gmail.com>
2017-10-04 10:20:50 -0700

    make test and constructor

--- test/store.test.js
+++ test/store.test.js
@@ -1,4 +1,21 @@
 'use strict';
 
+const assert = require('assert');
 const Store = require('../lib/store');
 
+let testStore = {};
+
+describe('Store class', () => {
+
+    beforeEach(() => {
+        testStore = new Store();
+    });
+
+    describe('constructor', () => {
+        it('creates an object with access to empty warehouse property', () => {
+            assert.deepEqual(testStore.warehouse, {});
+        });
+    });
+
+
+});
