917c21419f9e1dde1cd7c08ff29f906b443c9d55
Maryussuperboy <martsyalis@gmail.com>
2017-10-04 13:14:44 -0700

    removed console.logs

--- store.js
+++ store.js
@@ -23,13 +23,10 @@ class Store {
 
     remove(deleteId) {
         const originalLength = this.storesList.length;
-        console.log('storesList in the beggining', this.storesList);
         const deleteIndex = this.storesList.findIndex((value) =>{
             return value._id === deleteId;
         });
-        console.log('deleteIndex', deleteIndex);
         this.storesList.splice(deleteIndex,1);
-        console.log('storesList after slice', this.storesList);
         if (originalLength === this.storesList.length) {
             return { removed: false };
         } else {
