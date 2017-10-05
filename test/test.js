67be7a45bde4c5bdf14f74638150252814794ef7
Christina Mills <chrimills@gmail.com>
2017-10-04 18:49:22 -0700

    finally done, all test are working or I have started halucinating

--- test/test.js
+++ test/test.js
@@ -1,28 +1,73 @@
 const assert = require('assert');
 const Store = require('../lib/store');
 
-describe('whole store maybe?', () => {
+describe('store', () => {
+    let store = null;
     const book = { name: 'Harry Potter'};
-    let bookStore = null;
+
     beforeEach(() => {
-        bookStore = new Store(book);
-        
+        store = new Store();
     });
 
-    describe('saves object w/id', () => {
-        it('saves something', () => {
-            bookStore.save(); //sim user imput
-            assert.ok(bookStore.book._id);
+    describe('Save', () => {
+        it('saves an object with an id', () => {
+            let saved = store.save(book);
+            
+            assert.ok(saved._id);
         });
 
     });
 
-    describe('gets object by id', () => {
-        it.only('finds something by id', () => {
-            const savedBook = bookStore.save(); 
-            const gotBook = bookStore.get(savedBook); //actual-results- what meth returned
-            assert.deepEqual(gotBook, savedBook._id); //actual and expected
+    describe('Get', () => {
+        it('gets object by id', () => {
+            const saved = store.save(book);
+            let objectId = store.get(saved._id);
+
+            assert.equal(objectId, book);
+
         });
+
+        it('gets object without an id', () => {
+            let objectNoID = store.get();
+
+            assert.equal(objectNoID, null);
+
+        });
+
     });
+
+    describe('Get All', () => {
+        it('returns all of the objects', () => {
+            let newObjectArray = store.getAll();
+
+            assert.deepEqual(store.storeList, newObjectArray );
+        });
+
+        it('return empty object array', () => {
+            let emptyArray = store.getAll([]);
+
+            assert.deepEqual(emptyArray, []);
+        });
     
+    });
+
+    describe('removes id', () => {
+        it('removes the id from an an object', () => {
+            store.save(book);
+            let removeById = store.remove(book._id);
+
+            assert.deepEqual(removeById, { removed: true } );
+        });
+
+        it('returns false when Id of obj to remove doesnt exist', () => {
+            let removeStatus = store.remove('does not exist');
+
+            assert.deepEqual(removeStatus, { removed: false });
+        });
+
+    });
+
 });
+
+   
+   
