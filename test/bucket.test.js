b5d503fea8d3e76fdbef521bea9a4d62a0903772
mugworks <greenwood.pdx@gmail.com>
2017-10-07 09:18:22 -0700

    tests remove object and improves getAll test

--- test/bucket.test.js
+++ test/bucket.test.js
@@ -7,15 +7,18 @@ const Bucket = require('../lib/bucket');
 describe ('testing constructor function', () => {
     const food = 'salad';
     let bucket = null;
+
     beforeEach(function () {
         bucket = new Bucket();
     });
     
+
     it ('should create an id property', () => {
         const obj = bucket.save(food);
         const id = obj._id;
         assert.ok(id); 
     });
+
 
     it ('should check to make sure the object was saved', () => {
         const obj = bucket.save(food);
@@ -23,17 +26,16 @@ describe ('testing constructor function', () => {
         assert.deepEqual(getObj, obj);
     });
 
-    // it ('should return the object that has that id', () => {
-
-    // });
+    
 
     it ('should return null if the id does not exist', () => {
-        const obj = bucket.save(food);
+        bucket.save(food);
         const getObj = bucket.get(66); //assigning a random id number
         assert.equal(getObj, null);
     });
 
-    it.skip ('should return an array of all objects', () => {
+
+    it.only ('should return an array of all objects', () => {
         const food2 = 'pizza';
         const food3 = 'beer';
         bucket.save(food);
@@ -41,26 +43,32 @@ describe ('testing constructor function', () => {
         bucket.save(food3);
 
         const getAllObj = bucket.getAll();
-        // assert.equal(getAllObj[0].name, 'salad');
+        const mappedArray = getAllObj.map((obj) => {
+            return obj.name;
+        });
+        assert.deepEqual(mappedArray, ['salad', 'pizza', 'beer']);
     });
+
 
     it ('should return an empty array from getAll when no objects', () => {
         const getAllObj = bucket.getAll();
         assert.deepEqual(getAllObj, []);
     });
+
+
+    it ('should return True if an object has been removed by id', () => {
+        const obj = bucket.save(food);
+        const remObj = bucket.remove(obj._id);
+        assert.deepEqual(remObj, {removed: true});
+        const getAllObj = bucket.getAll();
+        assert.deepEqual(getAllObj, []);
+    });
     
+
+    it ('should return False if obj being removed does not exist', () => {
+        const remObj = bucket.remove(67);
+        assert.deepEqual(remObj, {removed: false});
+    });
     
 });
 
-
-
-// * `.get(<id>)`
-// * returns the object that has that id
-// * return `null` if that id does not exist
-// * `.getAll()`
-// * returns array of all objects
-// * return empty array `[]` when no objects
-// * `.remove(<id>)`
-// * removes the object from the store that has that id
-// * return `{ removed: true }` if the object was removed, else return `{ removed: false }` if the 
-// object did not exist
